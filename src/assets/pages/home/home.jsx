acec0d7ee1d65b38cdb81731bed8eeff6d2e9c99
clement brossier <brossier.clement@proton.me>
2024-01-21 00:26:49 +0100

    chore: responsive projects

--- src/assets/pages/home/home.jsx
+++ src/assets/pages/home/home.jsx
@@ -10,28 +10,28 @@ function Home() {
     <>
       <Header />
       <main className="home">
-        <section className="work">
-          <h1>Projects</h1>
-          <div id="projects" className="projects">
-            {data[0] ? (
-              data[0].projects.map((item) => (
-                <Link
-                  className="project"
-                  key={item.id}
-                  to={`/project/${item.id}`}
-                >
-                  <img src={item.logo} alt="" />
-                  <section className="titled">
-                    <h3>{item.name}</h3>
-                    <p>{item.titled}</p>
-                  </section>
-                </Link>
-              ))
-            ) : (
-              <p>Error...</p>
-            )}
-          </div>
-        </section>
+        <h1>Skills</h1>
+        <div className="skills"></div>
+        <h1>Projects</h1>
+        <div id="projects" className="projects">
+          {data[0] ? (
+            data[0].projects.map((item) => (
+              <Link
+                className="project"
+                key={item.id}
+                to={`/project/${item.id}`}
+              >
+                <img src={item.logo} alt="" />
+                <section className="titled">
+                  <h3>{item.name}</h3>
+                  <p>{item.titled}</p>
+                </section>
+              </Link>
+            ))
+          ) : (
+            <p>Error...</p>
+          )}
+        </div>
       </main>
     </>
   );
