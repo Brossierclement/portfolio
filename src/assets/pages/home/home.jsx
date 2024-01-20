009e99e8f0e7fcfbf97353b2bcdc1f87fa0281ff
clement brossier <brossier.clement@proton.me>
2024-01-20 23:55:39 +0100

    chore: new format for projects

--- src/assets/pages/home/home.jsx
+++ src/assets/pages/home/home.jsx
@@ -2,16 +2,17 @@ import "../home/home.scss";
 import Header from "../../components/header/header";
 import { useContext } from "react";
 import { DataContext } from "../../../App";
+import { Link } from "react-router-dom";
+
 function Home() {
   const data = useContext(DataContext);
-  // console.log(data[0]);
   return (
     <>
       <Header />
       <main className="home">
         <section className="work">
           <h1>Projects</h1>
-          {/* <div id="projects" className="projects">
+          <div id="projects" className="projects">
             {data[0] ? (
               data[0].projects.map((item) => (
                 <Link
@@ -19,7 +20,7 @@ function Home() {
                   key={item.id}
                   to={`/project/${item.id}`}
                 >
-                  <img src={item.banner} alt="" />
+                  <img src={item.logo} alt="" />
                   <section className="titled">
                     <h3>{item.name}</h3>
                     <p>{item.titled}</p>
@@ -29,7 +30,7 @@ function Home() {
             ) : (
               <p>Error...</p>
             )}
-          </div> */}
+          </div>
         </section>
       </main>
     </>
